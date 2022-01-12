0b32afffba6ac989b5658761b6b0616715fb86d5
afeexclusive <afeexclusive@gmail.com>
2022-01-12 15:40:00 +0100

    lawal

---
+++ APIHeaderImplementation/Controllers/HeaderApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileInternetBankingWebAPI.BAL.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIHeaderImplementation.Controllers
+{
+  //  [APIKey]
+    public class HeaderApiController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
